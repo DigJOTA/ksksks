3f7fc056a89bd7558233400a3e84f1eb4e5097e0
DigJOTA <161497751+DigJOTA@users.noreply.github.com>
2024-10-18 14:38:27 -0300

    FGD

---
+++ perguntas.js
@@ -0,0 +1,24 @@
+criaCartão(
+    'Programação',
+    'O que é Phyton?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartão(
+    'Geografia'
+    'Qual a capital da Rússia?'
+    'A capital da Rússsia é MOSCOW
+)
+
+    criaCartão(
+        'ANIMAL
+        'COMO SE CHAMA UM URSO ALBINO?',
+        'urso polar'
+    
+    )
+
+    criaCartão(
+        'FRUTA',
+        'Qual o gosto da maçã?'
+        'MAÇA TEM O MESMO GOSTO Q  A MAÇÃ
+    )'
